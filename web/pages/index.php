4f6b65aa894f690e090f022876bd1268eb724a00
RobinLKK <ormg.rob@gmail.com>
2026-05-12 11:59:45 +0200

    upd

--- web/pages/index.php
+++ web/pages/index.php
@@ -1,15 +1,1 @@
-<?php
-declare(strict_types=1);
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Clio2Tune</title>
-</head>
-<body>
-	<h1>Clio2Tune</h1>
-	<p>Simple PHP page.</p>
-</body>
-</html>
+// index 
---
+++ web/pages/leaderboard.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Clio2Tune</title>
+</head>
+<body>
+	<h1>Clio2Tune</h1>
+	<p>Page leaderboard.php</p>
+</body>
+</html>
