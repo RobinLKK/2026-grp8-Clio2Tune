f2c43f6554aa5db1ca8ec68808010645b89ef28f
NoeS-cir <noe.soccio@student.junia.com>
2026-05-12 10:47:58 +0200

    header et main

---
+++ grp8/2Fast4U/Header.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+typedef struct {
+	int ColorId; //Entier indiquant la zone Couleur 
+	bool hasCar; //booléen si true il y a une voiture si false il n'y en a pas
+	bool hasX; //booléen si true il y a une croix si false il n'y en a pas
+}Plateau;
+//Vérfie que l'on peut placer un objet en fonction de la ligne et colonne
+bool estPlacable(int ligne, int colonne);
+
+//Place les croix de manière automatique autour de la voiture et en croix autour d'elle
+void croixauto(int ligne, int colonne);
+
+//Quand le jouer Clique sur une case
+void CliqueCase(int ligne, int colonne);
+
+//
