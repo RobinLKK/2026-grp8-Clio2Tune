4eecdd7b752ef20c9f4f2bae38ac9c3e085792bb
RobinLKK <ormg.rob@gmail.com>
2026-05-20 15:29:49 +0200

    updt index

--- web/pages/index.php
+++ web/pages/index.php
@@ -11,6 +11,7 @@
     <script src="../js/regles.js" defer></script>
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
     <link rel="icon" href="../media/ico-car.ico" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Barlow:wght@400;500;600&display=swap" rel="stylesheet">
 </head>
 <body>
     <header>
@@ -31,11 +32,10 @@
     </div>
     </header>
 
-    <main>
-        <h2>Clio 2 Tuné</h2>
-        <p>Allez vient jouer on est cool </p>
-        <a href="gamemodes.php" class="btn">Play</a>
-    </main>
+        <main>
+        <h1>2<span style="color:#e2b96f">FAST</span>4U</h1>
+        <a href="gamemodes.php" class="btn-play">Play</a>
+        </main>
 
     <div id="overlay" class="overlay">
         <div class="modal">
